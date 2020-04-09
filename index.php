8462c02017060fec376e6f50ff7cc025c3e89190
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-04-08 22:59:29 -0400

    Video 8

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
     $name='Carlos';
-    $apellido='Sepulveda'
+    $apellido='Sepulveda';
+    $jobs=[
+        [
+            'title'=>'PHP Developer',
+            'description'=>'This in an awesome job!!'
+        ],
+        [
+            'title'=>'Python Dev'
+        ],
+        [
+            'title'=>'Devops'
+        ]
+    ];
+
+    
 ?>
 
 
@@ -54,8 +68,8 @@
           <h3 class="border-bottom-gray" >Work Experience</h3>
           <ul>
             <li class="work-position">
-              <h5>PHP Developer</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
+              <h5><?php echo $jobs[0]['title'];?></h5>
+              <p><?php echo $jobs[0]['description'];?></p>
               <strong>Achievements:</strong>
               <ul>
                 <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
@@ -64,7 +78,7 @@
               </ul>
             </li>
             <li class="work-position">
-                <h5>PHP Developer</h5>
+                <h5><?php echo $jobs[1]['title'];?></h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
                 <strong>Achievements:</strong>
                 <ul>
@@ -74,7 +88,7 @@
                 </ul>
               </li>
               <li class="work-position">
-                  <h5>PHP Developer</h5>
+                  <h5><?php echo $jobs[2]['title'];?></h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
                   <strong>Achievements:</strong>
                   <ul>
