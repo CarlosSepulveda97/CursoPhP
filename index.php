fe0fd59547c6e0fe259630bd3635ef3eefd4585a
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-04-10 11:52:29 -0400

    Video 15

--- index.php
+++ index.php
@@ -1,42 +1,70 @@
 <?php
     $name='Carlos';
     $apellido='Sepulveda';
-    $limitMonths=12;
+    $limitMonths=2000;
     $jobs=[
         [
             'title'=>'PHP Developer',
             'description'=>'This in an awesome job!!',
             'visible'=>true,
-            'months'=>3
+            'months'=>12
         ],
         [
             'title'=>'Python Dev',
             'description'=>'This in an awesome job!!',
             'visible'=>false,
-            'months'=>1
+            'months'=>6
         ],
         [
             'title'=>'Devops',
             'description'=>'This in an awesome job!!',
             'visible'=>false,
-            'months'=>4
+            'months'=>16
         ],
         [
             'title'=>'Node Dev',
+            'description'=>'This in an awesome job!!',
             'visible'=>true,
-            'months'=>2
+            'months'=>8
         ],
         [
             'title'=>'Frontend Dev',
+            'description'=>'This in an awesome job!!',
             'visible'=>true,
-            'months'=>3
-        ]
-    
-?>
+            'months'=>10
+        ]];
+
+        function printJob($job){
+            
+            if ($job['visible']==false){
+                return;
+            }
+
+            echo '<li class="work-position">';
+            echo '<h5>' . $job['title'] . '</h5>';
+            echo '<p>' . $job['description'] . '</p>';
+            echo '<p>' . getDuration($job['months']) . '</p>';
+            echo '<strong>Achievements:</strong>';
+            echo '<ul>';
+            echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+            echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+            echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+            echo '</ul>';
+            echo '</li>';
+        }
+
+        function getDuration($months){
+            $years=floor($months/12);
+            $extraMonths=$months%12;
+            if ($years>=1){
+                return "$years years $months months";
+            }else{
+                return "$months months";
+            }
+        }
 
 
-
-
+    ?>
 <!doctype html>
 <html lang="en">
 
@@ -94,21 +122,7 @@
                     }
 
 
-                    if ($jobs[$idx]['visible']==false){
-                            continue;
-                        }
-
-                    echo '<li class="work-position">';
-                    echo '<h5>' . $jobs[$idx]['title'] . '</h5>';
-                    echo '<p>' . $jobs[$idx]['description'] . '</p>';
-                    echo '<p>' . $totalMonths . '</p>';
-                    echo '<strong>Achievements:</strong>';
-                    echo '<ul>';
-                    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-                    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-                    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-                    echo '</ul>';
-                    echo '</li>';
+                    printJob($jobs[$idx]);
                 }
             ?>
           </ul>
